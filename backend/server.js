264812baa9275273b757fa2cf0584549ecdc5924
aditi ray <aditiraysingh2005@gmail.com>
2026-09-05 14:32:46 +0530

    added in files

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173"
+}));
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
