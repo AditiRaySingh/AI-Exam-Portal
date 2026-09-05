138b7a35f8bf3aeff6a84e71f71e677f35dbc2d3
AditiRaySingh <aditiraysingh2005@gmail.com>
2026-09-05 20:32:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://ai-exam-portal-frontend.onrender.com"
+        "https://ai-exam-portal-2.onrender.com"
     ],
     credentials: true
 }));
