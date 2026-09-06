77ccdf7fbb5f7ad8ceca89855b9da6aeb4e43c9a
AditiRaySingh <aditiraysingh2005@gmail.com>
2026-09-06 17:49:20 +0530

    Update TeacherDashboard.jsx

--- frontend/src/pages/TeacherDashboard.jsx
+++ frontend/src/pages/TeacherDashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "../styles/teacherDashboard.css";
+import "../styles/TeacherDashboard.css";
 
 function TeacherDashboard() {
 
